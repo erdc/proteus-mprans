5067fcee25e831efc16887d1b187c0a869952541
ido <ido@69ec0f36-a0ee-0310-8cb8-e62ac6c78490>
2010-10-12 01:37:00 +0000

    Run directory for hex marin
    git-svn-id: https://adh.usace.army.mil/svn/pyadh/branches/proteus/models/mprans@3864 69ec0f36-a0ee-0310-8cb8-e62ac6c78490

---
+++ 3d/marinHex/ls_consrv_obstacleInTank_3d_n.py
@@ -0,0 +1,54 @@
+from proteus import *
+from proteus.default_n import *
+from obstacleInTank3d import *
+from ls_consrv_obstacleInTank_3d_p import *
+
+
+stepController = FixedStep
+timeIntegration = NoIntegration
+
+femSpaces = {0:C0_AffineLinearOnCubeWithNodalBasis}
+
+elementQuadrature = CubeGaussQuadrature(nd,obstacleInTank_quad_order)
+
+elementBoundaryQuadrature = CubeGaussQuadrature(nd-1,obstacleInTank_quad_order)
+
+subgridError = None
+
+massLumping = False
+
+numericalFluxType = DoNothing
+
+shockCapturing = None
+
+multilevelNonlinearSolver  = NLNI
+
+levelNonlinearSolver = Newton
+
+nonlinearSmoother = NLGaussSeidel
+
+fullNewtonFlag = True
+
+tolFac = 0.001
+
+nl_atol_res = 0.0
+
+maxNonlinearIts = 10
+maxLineSearches =0
+
+matrix = SparseMatrix
+
+if usePETSc:
+    multilevelLinearSolver = PETSc #KSP_petsc4py
+    levelLinearSolver = PETSc #KSP_petsc4py
+    linear_solver_options_prefix = 'mcorr_'
+    linearSmoother = None
+    #linearSolverConvergenceTest = 'r-true'
+else:
+    multilevelLinearSolver = LU
+    levelLinearSolver = LU
+
+
+linTolFac = 1.0e-6
+
+conservativeFlux = None
---
+++ 3d/marinHex/vof_obstacleInTank_3d_n.py
@@ -0,0 +1,53 @@
+from proteus import *
+from proteus.default_n import *
+from obstacleInTank3d import *
+from vof_obstacleInTank_3d_p import *
+
+elementQuadrature = CubeGaussQuadrature(nd,obstacleInTank_quad_order)
+
+elementBoundaryQuadrature = CubeGaussQuadrature(nd-1,obstacleInTank_quad_order)
+
+
+timeIntegration = BackwardEuler_cfl
+stepController = FixedStep
+femSpaces = {0:C0_AffineLinearOnCubeWithNodalBasis}
+shockCapturing = ResGradQuad_SC(coefficients,nd,shockCapturingFactor=vof_shockCapturingFactor,lag=lag_vof_shockCapturing)#linear
+subgridError = Advection_ASGS(coefficients=coefficients,nd=nd,lag=False)#it's linear anyway
+massLumping = False
+numericalFluxType = Advection_DiagonalUpwind_IIPG_exterior
+
+multilevelNonlinearSolver  = Newton #NLNI
+
+levelNonlinearSolver = Newton
+
+nonlinearSmoother = NLGaussSeidel
+
+fullNewtonFlag = True
+
+tolFac = 0.0001
+
+nl_atol_res = 0.0
+
+maxNonlinearIts = 10
+
+matrix = SparseMatrix
+
+nonlinearSolverConvergenceTest = 'rits'
+levelNonlinearSolverConvergenceTest = 'rits'
+
+if usePETSc:
+     multilevelLinearSolver = PETSc #KSP_petsc4py
+     levelLinearSolver = PETSc #KSP_petsc4py
+     linear_solver_options_prefix = 'vof_'
+     #linearSmoother = None
+     linearSmoother = None ##StarILU
+     #linearSolverConvergenceTest = 'r-true'
+     nonlinearSolverConvergenceTest='rits'
+     levelNonlinearSolverConvergenceTest='rits'
+else:
+    multilevelLinearSolver = LU
+    levelLinearSolver = LU
+
+linTolFac = 0.001
+
+conservativeFlux = None
