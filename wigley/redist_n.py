26cb111bdd71857154add3631a5aa0a849b4f9b5
ido <ido@69ec0f36-a0ee-0310-8cb8-e62ac6c78490>
2011-03-10 20:01:06 +0000

    New runfiles for wigley case
    git-svn-id: https://adh.usace.army.mil/svn/pyadh/branches/proteus/models/mprans@4193 69ec0f36-a0ee-0310-8cb8-e62ac6c78490

---
+++ wigley/redist_n.py
@@ -0,0 +1,50 @@
+from proteus import *
+from proteus.default_n import *
+from wigley import *
+from redist_p import *
+
+timeIntegration = BackwardEuler
+stepController = Osher_PsiTC_controller2
+runCFL=1.0
+#rtol_res[0] = 1e-3
+#atol_res[0] = 0.0
+
+femSpaces = {0:C0_AffineLinearOnSimplexWithNodalBasis}
+
+elementQuadrature = SimplexGaussQuadrature(nd,quad_order)
+
+elementBoundaryQuadrature = SimplexGaussQuadrature(nd-1,quad_order)
+
+subgridError = HamiltonJacobi_ASGS_opt(coefficients,nd,stabFlag='2',lag=True)    
+shockCapturing = ResGradQuad_SC(coefficients,nd,shockCapturingFactor=False,lag=True)
+
+massLumping = False
+
+numericalFluxType = DoNothing
+
+multilevelNonlinearSolver  = Newton
+levelNonlinearSolver = Newton
+
+nl_atol_res = 10.0
+maxNonlinearIts = 1
+maxLineSearches = 0
+psitc['nStepsForce']=5
+psitc['startRatio'] = 10.0 
+psitc['reduceRatio']= 1.0
+psitc['nStepsMax']=10
+nonlinearSolverConvergenceTest = 'rits'
+levelNonlinearSolverConvergenceTest = 'rits'	
+        
+nonlinearSmoother = None
+linearSmoother = None
+
+matrix = SparseMatrix
+
+multilevelLinearSolver = PETSc
+levelLinearSolver = PETSc
+
+linear_solver_options_prefix = 'rdls_'
+
+linTolFac = 0.001
+
+conservativeFlux = None
--- wigley/wigley.py
+++ wigley/wigley.py
@@ -3,133 +3,87 @@ A helper module for doing air/water flow around a moving rigid cylinder in 2D
 """
 from math import *
 from proteus import *
-checkMass=False
-#
-#material properties
-#
-#water
+from mpi4py import MPI
+
+#----------------------------------------------------
+# Physical properties
+#----------------------------------------------------
 rho_0=998.2
-nu_0=1.004e-6
-#air
+nu_0 =1.004e-6
+
 rho_1=1.205
-nu_1= 1.500e-5# *1.0e5#cek hack damp turbulence in air phase
-sigma_01=0.0#72.8e-3
-#gravity
+nu_1 =1.500e-5
+
+sigma_01=0.0
+
 g=[0.0,0.0,-9.81]
-useStokes=False
-#
-#domain
-#
+
+#----------------------------------------------------
+# Domain - mesh - quadrature
+#----------------------------------------------------
 nd = 3
-hull_beam = 0.238
-hull_draft = 0.095
-hull_length=  1.0
-height=2.0*hull_draft#2.0*(3.0*hull_draft)
-length=2.0*hull_length#
-#length = 4.0*hull_length+hull_length
-width=4.0*hull_length+hull_beam
-width=3.0*hull_beam
-height=3.0*hull_draft
-length=2.0*hull_length
-#width=4.0*hull_beam#4.0*hull_length+hull_beam
-#
-#width*=8.0
-#length*=4.0
-#
-#he = 0.5*hull_draft#length
-he = hull_draft#length
-width=2.0*hull_beam
-he = 0.01#5*hull_draft
-n_points_draft=max(2,int(ceil(hull_draft/he))) + 1
-n_points_length=max(2,int(ceil(hull_length/he))) + 1
+hull_beam   = 0.238
+hull_draft  = 0.095
+hull_length = 1.000
+waterLevel  = 0.500 
 
-from wigley3dDomain import *
-genMesh=False
+nLevels = 1
+domain = Domain.MeshTetgenDomain(fileprefix="wigley")
+boundaryTags = { 'bottom': 1, 'front':2, 'right':3, 'back': 4, 'left':5, 'top':6, 'obstacle':11}
 
-meshname="DTMB"
-#meshname="wigley"
-domain = wigley3d(meshname,
-                  height,
-                  length,
-                  width,
-                  hull_beam,
-                  hull_draft,
-                  hull_length,
-                  (0.65*hull_length,0.5*width,0.5*height),
-                  n_points_draft,
-                  n_points_length)
-domain.writePoly(meshname)
-domain.writePLY(meshname)
-domain.writeAsymptote(meshname)
-boundaryTags = domain.boundaryTags
-print boundaryTags
-waterLevel = 0.258016 
+restrictFineSolutionToAllMeshes=False
+parallelPartitioningType = MeshTools.MeshParallelPartitioningTypes.element
+nLayersOfOverlapForParallel = 0
+
+quad_order = 2
+
+#----------------------------------------------------
+# Boundary conditions and other flags
+#----------------------------------------------------
 openTop = True
-openSides = False#True
+openSides = True
 smoothBottom = False
 smoothObstacle = False
 rampInitialConditions = False
-#
-# openTop = False
-# openSides = False
-# smoothBottom = True
-# smoothObstacle = False
-movingDomain=False#True
-#
-#residence time based on mean velocity
-#
-Um = 1.0#m/s 10 knots
-RE = hull_beam*Um/nu_0
-#RE = 4.2e6
-#Um = RE*nu_0/hull_length
-Fr = Um/math.sqrt(math.fabs(g[2])*hull_length)
-Frh = Um/math.sqrt(math.fabs(g[2])*waterLevel)
-print "========================================REYNOLDS NUMBER = "+`RE`
-print "========================================FROUDE(HULL LENGTH) NUMBER = "+`Fr`
-print "========================================FROUDE(DEPTH) NUMBER = "+`Frh`
-print "========================================SPEED[M/S] = "+`Um`
-residence_time = length/Um
-#
-#time interval etc.
-#
-dt_init=1e-2#0.001*residence_time
-T = 5.0*residence_time
-print "========================================residence time = "+`residence_time`
-print "========================================T = "+`T`
-nDTout=1000
-runCFL = 0.33
-#
-#numerics
-#
-nLevels = 1
-genMesh=False
 
-boundaryTags = { 'bottom': 1, 'front':2, 'right':3, 'back': 4, 'left':5, 'top':6, 'obstacle':11}
-domain.boundaryTags = boundaryTags
-triangleOptions="VApq1.25q13fena%f" % ((he**3)/6.0,)
-print triangleOptions
+movingDomain=False
 
-
-useRBLES = 0.0
-useMetrics = 0.0
-
-
+checkMass=False
 applyCorrection=True
 applyRedistancing=True
-rdtimeIntegration='osher'
 freezeLevelSet=True
-quad_order = 2
-useBackwardEuler=True
-useBackwardEuler_ls=True
-#subgrid error
-lag_ns_subgridError=False
-lag_ns_shockCapturing=False
-lag_ls_shockCapturing=False
-#shock capturing diffusion
-ns_shockCapturingFactor=0.2
-ns_shockCapturingFactor=0.2
-ls_shockCapturingFactor=0.2
 
+#----------------------------------------------------
+# Time stepping and velocity
+#----------------------------------------------------
+Fr = 0.28
+Um = Fr*sqrt(fabs(g[2])*hull_length)
+Re = hull_beam*Um*rho_0/nu_0
+
+residence_time = hull_length/Um
+dt_init=0.02
+T = 5.0*residence_time
+
+nDTout=int(ceil(T/dt_init))
+
+if MPI.COMM_WORLD.Get_rank() == 0:
+	print "   ================   REYNOLDS NUMBER = "+`Re`
+	print "   ================   FROUDE(HULL LENGTH) NUMBER = "+`Fr`
+	print "   ================   SPEED[M/S] = "+`Um`
+	print "   ================   Hull Flow time = "+`residence_time`
+	print "   ================   T = "+`T`
+	print "   ================   nDTout = "+`nDTout`
+
+#----------------------------------------------------
+# Numerical parameters
+#----------------------------------------------------
+
+useRBLES   = 0.0
+useMetrics = 0.0
+ns_shockCapturingFactor=0.2
+ns_shockCapturingFactor=0.2
+
+ls_shockCapturingFactor=0.2
 ls_sc_uref = 1.0
 ls_sc_beta = 1.5
 
@@ -138,17 +92,23 @@ vof_sc_uref = 1.0
 vof_sc_beta = 1.5
 
 rd_shockCapturingFactor=0.2
-#epsilons for Heaviside/Dirac/etc smoothing
-epsFact_density = 1.5
-epsFact_viscosity = 1.5
-epsFact_redistance = 0.33
-epsFact_curvature=1.5
-epsFact_consrv_heaviside=1.5
-epsFact_consrv_dirac=1.5
-epsFact_consrv_diffusion=50.0
-epsFact_vof=1.5
-#
 
-restrictFineSolutionToAllMeshes=False
-parallelPartitioningType = MeshTools.MeshParallelPartitioningTypes.element
-nLayersOfOverlapForParallel = 0
+epsFact_consrv_diffusion=50.0
+
+#----------------------------------------------------
+# Interface width
+#----------------------------------------------------
+epsFact = 1.5
+
+epsFact_redistance = 0.33
+
+epsFact_density          = epsFact 
+epsFact_viscosity        = epsFact 
+epsFact_redistance       = epsFact 
+epsFact_curvature        = epsFact 
+epsFact_consrv_heaviside = epsFact 
+epsFact_consrv_dirac     = epsFact 
+epsFact_vof              = epsFact 
+
+
+
