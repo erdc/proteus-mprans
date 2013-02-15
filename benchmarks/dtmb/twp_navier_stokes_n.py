ef4cfa0b982810f4083fde30a2dc02da66186fc7
Chris Kees <cekees@gmail.com>
2013-02-15 11:22:48 -0600

    updated the dtmb input files

--- benchmarks/dtmb/twp_navier_stokes_n.py
+++ benchmarks/dtmb/twp_navier_stokes_n.py
@@ -1,12 +1,9 @@
 from proteus import *
 from twp_navier_stokes_p import *
-from dtmb import *
-
-timeIntegration = BackwardEuler
-stepController  = Min_dt_controller
+from wigley import *
 
 timeIntegration = BackwardEuler_cfl
-stepController = Min_dt_controller
+stepController  = Min_dt_controller
 
 femSpaces = {0:basis,
 	     1:basis,
@@ -26,7 +23,7 @@ class NumericalFluxType(NavierStokes_Advection_DiagonalUpwind_Diffusion_SIPG_ext
         NavierStokes_Advection_DiagonalUpwind_Diffusion_SIPG_exterior.__init__(self,vt,getPointwiseBoundaryConditions,
                                                                                getAdvectiveFluxBoundaryConditions,
                                                                                getDiffusiveFluxBoundaryConditions,getPeriodicBoundaryConditions)
-        self.penalty_constant = 2.0
+        self.penalty_constant = 100.0
         self.includeBoundaryAdjoint=True
         self.boundaryAdjoint_sigma=1.0
         self.hasInterior=False
@@ -38,8 +35,6 @@ shockCapturing = NavierStokes_SC_opt(coefficients,nd,ns_shockCapturingFactor,lag
 fullNewtonFlag = True
 multilevelNonlinearSolver = NewtonNS
 levelNonlinearSolver      = NewtonNS
-multilevelNonlinearSolver = Newton
-levelNonlinearSolver      = Newton
 
 nonlinearSmoother = None
 linearSmoother    = SimpleNavierStokes3D
@@ -56,14 +51,14 @@ else:
 if useSuperlu:
     multilevelLinearSolver = LU
     levelLinearSolver      = LU
-    
+
 linear_solver_options_prefix = 'rans2p_'
-levelNonlinearSolverConvergenceTest = 'rits'
-linearSolverConvergenceTest         = 'r-true'
+levelNonlinearSolverConvergenceTest = 'r'
+linearSolverConvergenceTest             = 'r-true'
 
-tolFac = 0.0
-nl_atol_res = 1.0e-3
-
+tolFac = 1.0e-3
+nl_atol_res = 1.0e-4
+useEisenstatWalker = True
 maxNonlinearIts = 20
 maxLineSearches = 0
 
